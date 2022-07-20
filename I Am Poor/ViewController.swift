48aaa65c708a01d31d2e38a1344f86f290f74b83
Elif Bihter KUŞÇU <56038735+bihterkuscu@users.noreply.github.com>
2022-07-20 14:40:41 +0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Elif Bihter Kuşçu on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
